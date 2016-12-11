fe6793c748e127e3db3259f89277d80cdf1e43bc
Jithin Devasia <jithindevasia@gmail.com>
2016-12-11 17:01:40 +0530

    File formated and added email

--- ascending.sh
+++ ascending.sh
@@ -1,5 +1,17 @@
+# ---------------------------------
+#	Written by Jithin Devasia
+#	jithindevasia@gmail.com
+# ---------------------------------
+# This script will prrint asterisk symbol (*) in an ascending order pattern based on the input you given.
+#	*
+#	* *
+#	* * *
+#	* * * * 
+#	* * * * *
+
 #!/bin/bash
-n=5
+echo "Enter the number of rows you would like to create : "
+read n
 for (( i = 0; i < n; i++ )); do
 	for (( j = 0; j <= i; j++ )); do
 		echo -n "* "
